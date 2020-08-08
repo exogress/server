4e33733764452768b46ce96413c799973544c572
Gleb Pomykalov <gleb@exogress.com>
2020-08-08 17:21:08 +0300

    WIP

--- crates/signaler/src/http/public.rs
+++ crates/signaler/src/http/public.rs
@@ -24,7 +24,7 @@ pub async fn server(
     stop_handle: StopHandle<StopReason>,
     stop_wait: StopWait<stop_handle::StopReason<StopReason>>,
 ) {
-    let presence = warp::path!("api" / "channel" / String)
+    let presence = warp::path!("api" / "v1" / "instances" / String / "channel")
         .and_then(|instance_id: String| async move {
             match instance_id.parse::<InstanceId>() {
                 Err(_e) => Err(warp::reject::custom(BadInstanceId {})),
