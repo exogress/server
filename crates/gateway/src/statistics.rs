0d2d6bbde61beea5aa78debc9b6828963279e4de
Gleb Pomykalov <gleb@exogress.com>
2020-12-18 17:02:16 +0300

    add prefix to all metrics

--- crates/gateway/src/statistics.rs
+++ crates/gateway/src/statistics.rs
@@ -10,65 +10,67 @@ lazy_static! {
         10000.0, 20000.0
     ];
     pub static ref CONFIGS_CACHE_HIT: IntCounter =
-        register_int_counter!("configs_cache_hit", "Configs cache hit").unwrap();
+        register_int_counter!("gw_configs_cache_hit", "Configs cache hit").unwrap();
     pub static ref CONFIGS_CACHE_MISS: IntCounter =
-        register_int_counter!("configs_cache_miss", "Configs cache miss").unwrap();
+        register_int_counter!("gw_configs_cache_miss", "Configs cache miss").unwrap();
     pub static ref CONFIGS_RETRIEVAL_TIME_MS: Histogram = register_histogram!(
-        "configs_retrieval_time_ms",
+        "gw_configs_retrieval_time_ms",
         "Time take to retrieve the config (in milliseconds)",
         API_REQUEST_TIME_HISTOGRAM.clone()
     )
     .unwrap();
     pub static ref CONFIGS_RETRIEVAL_SUCCESS: IntCounter = register_int_counter!(
-        "configs_retrieval_success",
+        "gw_configs_retrieval_success",
         "Number of successful configs retrieval (incl. not-found)"
     )
     .unwrap();
     pub static ref CONFIGS_RETRIEVAL_ERROR: IntCounterVec = register_int_counter_vec!(
-        "configs_retrieval_error",
+        "gw_configs_retrieval_error",
         "Number of erroneous configs retrievals",
         &["error", "status_code"]
     )
     .unwrap();
     pub static ref CONFIGS_FORGOTTEN: IntCounter =
-        register_int_counter!("configs_forgotten", "Number of forgotten configs").unwrap();
+        register_int_counter!("gw_configs_forgotten", "Number of forgotten configs").unwrap();
     pub static ref CERTIFICATES_CACHE_HIT: IntCounter =
-        register_int_counter!("certificates_cache_hit", "Certificates cache hit").unwrap();
+        register_int_counter!("gw_certificates_cache_hit", "Certificates cache hit").unwrap();
     pub static ref CERTIFICATES_CACHE_MISS: IntCounter =
-        register_int_counter!("certificates_cache_miss", "Certificates cache miss").unwrap();
+        register_int_counter!("gw_certificates_cache_miss", "Certificates cache miss").unwrap();
     pub static ref CERTIFICATES_RETRIEVAL_TIME_MS: Histogram = register_histogram!(
-        "certificates_retrieval_time_ms",
+        "gw_certificates_retrieval_time_ms",
         "Time take to retrieve the certificate (in milliseconds)",
         API_REQUEST_TIME_HISTOGRAM.clone()
     )
     .unwrap();
     pub static ref CERTIFICATES_RETRIEVAL_SUCCESS: IntCounter = register_int_counter!(
-        "certificates_retrieval_success",
+        "gw_certificates_retrieval_success",
         "Number of successful certificates retrieval (incl. not-found)"
     )
     .unwrap();
-    pub static ref CERTIFICATES_FORGOTTEN: IntCounter =
-        register_int_counter!("certificates_forgotten", "Number of forgotten certificates")
-            .unwrap();
+    pub static ref CERTIFICATES_FORGOTTEN: IntCounter = register_int_counter!(
+        "gw_certificates_forgotten",
+        "Number of forgotten certificates"
+    )
+    .unwrap();
     pub static ref CERTIFICATES_RETRIEVAL_ERROR: IntCounterVec = register_int_counter_vec!(
-        "certificates_retrieval_error",
+        "gw_certificates_retrieval_error",
         "Number of erroneous certificates retrievals",
         &["error", "status_code"]
     )
     .unwrap();
     pub static ref TUNNELS_GAUGE: Gauge =
-        register_gauge!("tunnels", "Number of tunnels with clients").unwrap();
+        register_gauge!("gw_connected_tunnels", "Number of tunnels with clients").unwrap();
     pub static ref TUNNELS_BYTES_SENT: IntCounter =
-        register_int_counter!("tunnels_bytes_sent", "Bytes sent to tunnels").unwrap();
+        register_int_counter!("gw_tunnels_bytes_sent", "Bytes sent to tunnels").unwrap();
     pub static ref TUNNELS_BYTES_RECV: IntCounter =
-        register_int_counter!("tunnels_bytes_recv", "Bytes received from tunnels").unwrap();
+        register_int_counter!("gw_tunnels_bytes_recv", "Bytes received from tunnels").unwrap();
     pub static ref HTTPS_BYTES_SENT: IntCounter = register_int_counter!(
-        "serve_bytes_sent",
+        "gw_https_bytes_sent",
         "Bytes sent through HTTPS (serving traffic)"
     )
     .unwrap();
     pub static ref HTTPS_BYTES_RECV: IntCounter = register_int_counter!(
-        "https_bytes_recv",
+        "gw_https_bytes_recv",
         "Bytes received from HTTPS (serving traffic)"
     )
     .unwrap();
