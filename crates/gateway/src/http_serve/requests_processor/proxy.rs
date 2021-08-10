879d9ae0539326d048bbd740eae7486971c4ab49
Gleb Pomykalov <gleb@exogress.com>
2021-08-10 22:56:40 +0300

    ws logging

--- crates/gateway/src/http_serve/requests_processor/proxy.rs
+++ crates/gateway/src/http_serve/requests_processor/proxy.rs
@@ -96,15 +96,19 @@ impl ResolvedProxy {
         let mut is_websocket = false;
 
         if let Some(upgrade) = req.headers().get(UPGRADE) {
+            info!("upgrade = {:?}", upgrade);
             if upgrade
                 .to_str()
                 .unwrap()
                 .to_lowercase()
                 .contains("websocket")
             {
+                info!("it is ws");
                 if self.is_websockets_enabled {
+                    info!("ws enabled! use it");
                     is_websocket = true;
                 } else {
+                    info!("throw exception");
                     return HandlerInvocationResult::Exception {
                         name: exceptions::PROXY_WEBSOCKETS_DISABLED.clone(),
                         data: Default::default(),
@@ -119,8 +123,10 @@ impl ResolvedProxy {
         connect_target.update_url(&mut proxy_to);
 
         if proxy_to.scheme().unwrap() == "https" {
+            info!("https->http");
             proxy_to.set_scheme("http");
         } else if proxy_to.scheme().unwrap() == "wss" {
+            info!("wss -> ws");
             proxy_to.set_scheme("ws");
         } else {
             unreachable!("unknown scheme: {}", proxy_to);
@@ -174,6 +180,8 @@ impl ResolvedProxy {
                     });
 
                     let proxy_res = if is_websocket {
+                        info!("proxying WS!");
+
                         let mut ws_req = http::Request::new(());
 
                         *ws_req.headers_mut() = proxy_req.headers().clone();
@@ -219,6 +227,8 @@ impl ResolvedProxy {
                         *req.headers_mut() = req_for_upgrade.headers().clone();
                         *req.uri_mut() = req_for_upgrade.uri().clone();
                         *req.method_mut() = req_for_upgrade.method().clone();
+
+                        info!("perform WS {:?}", req);
 
                         tokio::spawn(
                             #[allow(unreachable_code)]
